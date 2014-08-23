8e9ddced15281d44cf2ba0f6d9aa76319fb8e286
Grant Rodgers <grantr@gmail.com>
2014-08-23 15:36:30 -0700

    sort photos by created_at

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -4,7 +4,7 @@ class PhotosController < ApplicationController
   def index
     if params[:timestamp]
       timestamp = Time.zone.at(params[:timestamp].to_f)
-      @photos = Photo.with_deleted.where('updated_at >= ?', timestamp)
+      @photos = Photo.with_deleted.where('updated_at >= ?', timestamp).order(created_at: :asc)
     else
       @photos = Photo.all
     end
